4c3b1fd9b6285df9369f0d70199221802b5767ca
Ben Lubar <ben.lubar@gmail.com>
2016-09-16 11:31:58 -0500

    fix only one mention emoji being replaced between each HTML tag

--- library.js
+++ library.js
@@ -66,8 +66,10 @@ exports.parseRaw = function(raw, callback) {
 						return content;
 					}
 
-					return content.replace(new RegExp('((?:^|>)[^<]*)' + match, 'g'), function(all, prefix) {
-						return prefix + '<a class="plugin-mentions-emoji-a" href="' + nconf.get('url') + '/uid/' + uid + '"><img src="' + picture + '" class="plugin-mentions-emoji not-responsive" alt="' + match.slice(2, -1) + '" title="' + match.slice(2, -1) + '" width="20" height="20" /></a>';
+					var re = new RegExp('((?:^|>)[^<]*)' + match);
+
+					return content.replace(re, function recurse(all, prefix) {
+						return prefix.replace(re, recurse) + '<a class="plugin-mentions-emoji-a" href="' + nconf.get('url') + '/uid/' + uid + '"><img src="' + picture + '" class="plugin-mentions-emoji not-responsive" alt="' + match.slice(2, -1) + '" title="' + match.slice(2, -1) + '" width="20" height="20" /></a>';
 					});
 				});
 
